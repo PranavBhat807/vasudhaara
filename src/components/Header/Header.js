956caa80da7c88b7220b49a5a7d0c86fbb86ac9c
Pranav <pranavbhat807@gmail.com>
2026-02-02 13:27:32 +0530

    mobile screen ui update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -76,7 +76,7 @@ export default function Header() {
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem>
+        <NavbarItem className={cartCount === 0 ? "hidden sm:flex" : ""}>
           <Link href="/checkout" className="mr-4">
             <div className="relative">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" /><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /></svg>
@@ -88,7 +88,7 @@ export default function Header() {
             </div>
           </Link>
         </NavbarItem>
-        <NavbarItem className="hidden sm:flex">
+        <NavbarItem className={cartCount === 0 ? "flex" : "hidden sm:flex"}>
           <Button as={Link} color="primary" href="/shop" variant="flat" className="font-semibold">
             Shop Now
           </Button>
